57d0260730f30fce5374937f02914f80979b51b8
youssef laarossi <laarossi.youssef@gmail.com>
2022-05-20 08:42:45 -0400

    Testing...

--- src/test/java/http/request/HTTPRequestTest.java
+++ src/test/java/http/request/HTTPRequestTest.java
@@ -2,10 +2,16 @@ package http.request;
 
 import exception.NoSuchUrlException;
 import http.HTTPMethod;
+import io.writer.ContentWriter;
+import io.writer.JSONContentWriter;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HTTPRequestTest {
@@ -14,13 +20,20 @@ class HTTPRequestTest {
 
     @BeforeAll
     public void init() throws NoSuchUrlException {
-         this.httpRequest = RequestBuilder.connect("https://www.google.com")
-                 .method(HTTPMethod.GET)
-                 .content("plain/text")
-                 .timeout(50000)
-                 .header("HeaderTest", "TestData")
-                 .entity("data")
-                 .build();
+        Map<String, String> headers = new HashMap<>(), cookies = new HashMap<>();
+        headers.put("HeaderTest2", "dzada");
+        cookies.put("cookie2","cookie2");
+        this.httpRequest = RequestBuilder.connect("https://www.google.com")
+                .method(HTTPMethod.GET)
+                .content("plain/text")
+                .timeout(50000)
+                .header("HeaderTest", "TestData")
+                .entity("data")
+                .writer(new JSONContentWriter())
+                .cookie("cookie", "cookie1")
+                .headers(headers, true)
+                .cookies(cookies, true)
+                .build();
     }
 
     @Test
@@ -39,14 +52,32 @@ class HTTPRequestTest {
     }
 
     @Test
-    void writer(){
-        this.httpRequest.contentWriter = new
+    void writer() throws IOException {
+        this.httpRequest.call();
+        assert this.httpRequest.body.equals("\"data\"");
+    }
+
+    @Test
+    void jsonWriter() throws IOException {
+        this.httpRequest.setContentWriter(new JSONContentWriter());
+        this.httpRequest.call();
+        assert this.httpRequest.body.equals("\"data\"");
     }
 
     @Test
     void print(){
         String output = this.httpRequest.toString();
         assert output != null && !output.isEmpty();
+    }
+
+    @Test
+    void cookies(){
+        assert this.httpRequest.cookies.size() == 2;
+    }
+
+    @Test
+    void headers(){
+        assert this.httpRequest.headers.size() == 2;
     }
 
 }
