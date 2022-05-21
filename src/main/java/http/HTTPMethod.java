3ef10a0121ffa90b96ef18c610cb1b9f79ff4763
youssef laarossi <laarossi.youssef@gmail.com>
2022-05-21 11:58:59 -0400

    Update...

--- src/main/java/http/HTTPMethod.java
+++ src/main/java/http/HTTPMethod.java
@@ -3,8 +3,8 @@ package http;
 public enum HTTPMethod {
 
     GET("GET"),POST("POST"),PUT("PUT"),PATCH("PATCH"),DELETE("DELETE"),OPTION("OPTION");
-    String method;
 
+    String method;
     HTTPMethod(String method){
         this.method = method;
     }
